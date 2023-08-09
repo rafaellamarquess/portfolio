d6a6966df07b04c9fe7b895751b222567e1cf974
Rafaella <fariassrafaella@gmail.com>
2023-08-09 15:14:05 -0300

    portifolio atualizado

---
+++ script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+
+     let typed = new Typed(".typing", {
+         strings:["Rafaella Marques"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+
+   
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
