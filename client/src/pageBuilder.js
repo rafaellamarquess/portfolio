ab9bd6b8a8bb95df63e99bb9fa5e27534ae823c1
Rafaella <fariassrafaella@gmail.com>
2023-08-15 14:17:28 -0300

    formatação da bio

--- client/src/pageBuilder.js
+++ client/src/pageBuilder.js
@@ -5,6 +5,7 @@ const buildBio = () => {
     const textoBio = getBio().then( (responseBio)  => {
         const divBio = document.querySelector('#bio')
 
+        divBio.setAttribute ("class", "column right" )
         const paragrafoBio = document.createElement('p')
         paragrafoBio.innerHTML = responseBio
     
