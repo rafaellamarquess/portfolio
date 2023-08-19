fd8c64a66025b31c29dd4fd583fae131496cfb63
Rafaella <fariassrafaella@gmail.com>
2023-08-19 14:36:31 -0300

    implementando api do github

--- client/src/index.js
+++ client/src/index.js
@@ -28,3 +28,4 @@ let typed = new Typed(".typing", {
 
 
 });
+
--- client/src/pageBuilder.js
+++ client/src/pageBuilder.js
@@ -27,3 +27,35 @@ const listaBio = (divBio) => {
 }
 
 buildBio()
+
+
+
+const repositorios = document.querySelector(".project-content")
+const getApiGitHub = () => {
+    fetch('https://api.github.com/users/rafaellamarquess/repos').then(async res => {
+        if (!res.ok) {
+            throw new Error(res.status)
+        }
+
+        let data = await res.json();
+        data.map (item => {
+            let projetos = document.createElement('div')
+            projetos.innerHTML = `
+            <div class="project-content">
+            <div class="card" style="width: 18rem;">
+            <img src="Assets/wikimedieval.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text"> ${item.name}</p>
+            </div>
+            <div>
+            <span class= "language"><span class= "circle"></span>${item.language}</span>
+            <a href="${item.html_url}" target="_black">${item.html_url}</a>
+            </div>
+        </div>`
+            
+            repositorios.appendChild(projetos)
+        })
+    })
+}
+
+getApiGitHub()
